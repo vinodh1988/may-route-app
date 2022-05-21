5e8e3e3b1e29487aab9347a7e1b09d8efd008691
vinodh1988 <k.c.vinodh@hotmail.com>
2022-05-21 16:51:41 +0530

    i18n

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,12 +9,18 @@ import { HomeComponent } from './pages/home/home.component';
 import { AboutComponent } from './pages/about/about.component';
 import { ContactsComponent } from './pages/contacts/contacts.component';
 import { MaterialModule } from './material/material.module';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { QuotesTabComponent } from './pages/home/quotes-tab/quotes-tab.component';
 import { PersonComponent } from './pages/about/person/person.component';
 import { PeopleFormComponent } from './pages/about/people-form/people-form.component';
 import { PeoplefilterPipe } from './pipes/peoplefilter.pipe';
+import {TranslateModule, TranslateLoader} from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+
+export function HttpLoaderFactory(http: HttpClient) {
+  return new TranslateHttpLoader(http,'./assets/i18n/', '.json');
+}
 
 @NgModule({
   declarations: [
@@ -35,7 +41,14 @@ import { PeoplefilterPipe } from './pipes/peoplefilter.pipe';
     MaterialModule,
     HttpClientModule,
     FormsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    TranslateModule.forRoot({
+      loader: {
+          provide: TranslateLoader,
+          useFactory: HttpLoaderFactory,
+          deps: [HttpClient]
+      }
+  })
   ],
   providers: [],
   bootstrap: [AppComponent]
